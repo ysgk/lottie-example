c9cbc670a6b8ade0d63b7a61bbeab43b266da6a9
ysgk <ysgk@users.noreply.github.com>
2020-07-28 16:59:33 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useLottie from './useLottie'
+import animationData from './animation.json'
+
+const mockRequest = (checked: boolean) => {
+  return new Promise<{ checked: boolean }>((resolve) => {
+    window.setTimeout(() => {
+      resolve({ checked })
+    }, 400)
+  })
+}
 
 function App() {
+  const [checked, setChecked] = React.useState(false)
+  const { replay, render } = useLottie({
+    animationData,
+  })
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> =  async (e) => {
+    const checked = e.currentTarget.checked
+    // mock network connection
+    const result = await mockRequest(checked)
+    setChecked(result.checked)
+
+    if (result.checked) {
+      replay()
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +36,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {render()}
+        <input type={'checkbox'} checked={checked} onChange={handleChange}/>
       </header>
     </div>
   );
---
+++ src/useLottie.tsx
@@ -0,0 +1,85 @@
+import lottie, { AnimationConfigWithData, AnimationItem } from 'lottie-web'
+import React from 'react'
+
+type Options =  {
+  animationData: any,
+}
+
+const useLottie = (options: Options) => {
+  const { animationData } = options
+  const animationInstanceRef = React.useRef<AnimationItem | null>(null)
+  const animationContainerRef = React.useRef<HTMLDivElement | null>(null)
+
+  const replay = (): void => {
+    animationInstanceRef.current?.goToAndPlay(0)
+  }
+
+  const hide = (): void => {
+    animationInstanceRef.current?.hide()
+  }
+
+  const show = (): void => {
+    animationInstanceRef.current?.show()
+  }
+
+  React.useEffect(() => {
+    // Return if the container ref is null
+    if (animationContainerRef.current == null) {
+      return
+    }
+
+    // Destroy any previous instance
+    animationInstanceRef.current?.destroy()
+
+    // Build the animation configuration
+    const config: AnimationConfigWithData = {
+      loop: false,
+      autoplay: false,
+      animationData,
+      container: animationContainerRef.current,
+    }
+
+    // Save the animation instance
+    animationInstanceRef.current = lottie.loadAnimation(config)
+    hide()
+  }, [animationData])
+
+  React.useEffect(() => {
+    const listeners = [
+      { name: 'complete', handler: () => {
+        hide()
+        } },
+      { name: 'enterFrame', handler: () => {
+        show()
+        }},
+    ].filter(({ handler }) => !!handler)
+
+    if (listeners.length === 0) {
+      return
+    }
+    const deregisterList = listeners.map((listener) => {
+      animationInstanceRef.current?.addEventListener(listener.name as any, listener.handler as any)
+
+      return () => {
+        animationInstanceRef.current?.removeEventListener(listener.name as any, listener.handler as any)
+      }
+    })
+
+    // Deregister listeners on unmount
+    return () => {
+      deregisterList.forEach((deregister) => deregister())
+    }
+  }, [])
+
+  const render = () => {
+    return <div ref={animationContainerRef} style={{ width: 42, height: 42 }} />
+  }
+
+  return {
+    render,
+    replay,
+  }
+}
+
+
+export default useLottie
